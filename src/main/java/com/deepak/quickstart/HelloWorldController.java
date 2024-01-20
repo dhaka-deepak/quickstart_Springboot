fe3b2a701a509a4dbedba2ce5075168c148ac2fa
Deepak Dhaka <deepa.dhaka.7634@gmail.com>
2024-01-20 15:30:32 +0530

    first commit

---
+++ src/main/java/com/deepak/quickstart/HelloWorldController.java
@@ -0,0 +1,14 @@
+package com.deepak.quickstart;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloWorldController {
+
+    @GetMapping(path = "/hello")
+
+    public String helloworld(){
+        return "Hello World !";
+    }
+}
---
+++ src/main/java/com/deepak/quickstart/QuickstartApplication.java
@@ -0,0 +1,13 @@
+package com.deepak.quickstart;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication//this is a identifier to the springboot applcation or entry point to the spring application
+public class QuickstartApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QuickstartApplication.class, args);
+	}
+
+}
